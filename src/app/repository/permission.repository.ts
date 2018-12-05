4108c28a367617d746d5f5353e40edca040051e3
tuanpham <tuanpham@vnnginfotech.com>
2018-12-05 13:58:12 +0700

    .

---
+++ src/app/repository/permission.repository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Permission } from '../entity/permission.entity';
+
+@EntityRepository(Permission)
+class PermissionRepository extends Repository<Permission> {}
+export default PermissionRepository;
