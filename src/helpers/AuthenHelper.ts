21e3da312b25bd7bfc6cb02199a54150c41d5232
tuanpham <tuanpham@vnnginfotech.com>
2018-12-26 15:48:49 +0700

    .

--- src/helpers/AuthenHelper.ts
+++ src/helpers/AuthenHelper.ts
@@ -1,42 +1,47 @@
 import { AuthenHelper, DataHelper } from 'aluha-ezcode-helper';
-import { ForbiddenException } from '@nestjs/common';
+import { ForbiddenException, UnauthorizedException } from '@nestjs/common';
 // tslint:disable-next-line:variable-name
 const Enumerable = require('linq');
 export class Authen {
   static async checkPermission(action: any, roles: string[]) {
     try {
       const user = await AuthenHelper.getUser(null, action);
-      if (user && user.role && user.role.length) {
-        let controller = '';
-        let method = '';
-        if (roles.length > 0) {
-          const type = roles[0];
-          if (type === 'None') {
-            return true;
+      const flg = roles[0];
+      if (user && flg === 'Passed') {
+        return true;
+      } else if (user) {
+        if (user && user.role && user.role.length) {
+          let controller = '';
+          let method = '';
+          if (roles.length > 0) {
+            const type = roles[0];
+            if (type === 'None') {
+              return true;
+            }
+            controller = roles[0];
+            method = roles[1];
+          } else {
+            const url = action.request.path;
+            const urls = DataHelper.getContextUrl(url);
+            if (urls) {
+              controller = urls.controller;
+              method = urls.action;
+            }
           }
-          controller = roles[0];
-          method = roles[1];
-        } else {
-          const url = action.request.path;
-          const urls = DataHelper.getContextUrl(url);
-          if (urls) {
-            controller = urls.controller;
-            method = urls.action;
-          }
+          const enumerableData = Enumerable.from(user.role);
+          const getActions = enumerableData
+            .where(
+              x =>
+                x.Permission &&
+                (x.Permission.controller === controller &&
+                  x.Permission.method === method)
+            )
+            .firstOrDefault();
+          if (getActions) return true;
+          else throw new ForbiddenException('Permission denied!');
         }
-        const enumerableData = Enumerable.from(user.role);
-        const getActions = enumerableData
-          .where(
-            x =>
-              x.Permission &&
-              (x.Permission.controller === controller &&
-                x.Permission.method === method)
-          )
-          .firstOrDefault();
-        if (getActions) return true;
-        else throw new ForbiddenException('Permission denied!');
+        throw new UnauthorizedException('Permission denied!');
       }
-      return false;
     } catch (error) {
       return false;
     }
